e97fd1a1c99b134c5f5d4ce834fd823dc2b4b296
vmwork <49744511+vmwork@users.noreply.github.com>
2020-08-26 12:20:16 +0300

    oo

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,24 @@
-$(function () {
+(function () {
+const arrowButtonSlider = document.querySelectorAll('.arrow'),
+      arrowButtonRight = document.querySelector('.arrow-rigth'),
+      arrowButtonLeft = document.querySelector('.arrow-left'),
+      sliderContent = document.querySelector('.advantages-list'),
+      sliderLineScrolling = document.querySelector('.arrows__line2');
 
-});
+        arrowButtonSlider.forEach(btn => {
+            btn.addEventListener('click', (e) => {
+                if(btn.classList.contains('arrow-rigth')){
+                   sliderContent.style.right = 890 +'px';
+                   sliderLineScrolling.style.left = 412 +'px';
+                   btn.style.opacity = 0.3;
+                   arrowButtonLeft.style.opacity = 1;                
+                } else if(btn.classList.contains('arrow-left')){
+                    sliderContent.style.right = 0 +'px';
+                    sliderLineScrolling.style.left = 0 +'px';
+                    btn.style.opacity = 0.3;
+                    arrowButtonRight.style.opacity = 1;
+                }
+                
+            })
+        })
+})();
